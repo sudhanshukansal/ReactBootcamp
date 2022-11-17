73540def31a9fb7e2a551a18dcdf9d6c3f7900cb
sudhanshukansal <54151246+sudhanshukansal@users.noreply.github.com>
2022-11-17 17:10:56 +0530

    Add files via upload

---
+++ React/app.js
@@ -0,0 +1,3 @@
+const CreateHeading = React.createElement("h1", {}, "Hello World");
+const CreateRoot = ReactDOM.createRoot(document.getElementsByClassName("root")[0]);
+CreateRoot.render(CreateHeading);
